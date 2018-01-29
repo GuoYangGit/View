efbc7d5e1603ce558d5a01ad54d14ca61200f6af
郭杨 <guoyang_add@163.com>
2018-01-29 15:05:33 +0800

    进度条修改

--- app/src/main/java/com/fuqin/android/view/progressbar/ProgressBarActivity.java
+++ app/src/main/java/com/fuqin/android/view/progressbar/ProgressBarActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.fuqin.android.view.R;
 
 public class ProgressBarActivity extends AppCompatActivity {
